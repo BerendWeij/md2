db2cce703da0744c3fd07c1cc7681236cd994d8b
Berend Weij <info@berendweij.nl>
2015-03-23 17:17:31 +0100

    added prototyping chain code

--- oop/js/Animal.js
+++ oop/js/Animal.js
@@ -27,33 +27,40 @@ $(function() {
     dog.bla= 3;
     pietje.extra = 5;
 
-
+    // we maken een standaard variable voor de unique id
+    // elk object geven we een unique id zodat we kunnen zien of ze uniek zijn :)
     var uID = 0;
     function showPrototypeChain(target, indention)
     {
+        // heeft deze target al een unique id? Zo niet, aanmaken!
         if(!target.hasOwnProperty("uID"))
         {
             target.uID = uID++;
         }
-
+        // we console.loggen het ID van dit object
         console.log(indention + "object met id: " + target.uID);
+        // we loop-en over alle properties van ons 'target'
         for(var property in target)
         {
             if(property != "uID" && target.hasOwnProperty(property))
             {
+                // we loggen deze property als hij echt van 'target' is (en niet van zijn prototype)
                 console.log(indention + "-- " + property + " (" + (typeof target[property]) + ")");
             }
         }
         indention += "   ";
+        // recursieve functie. We willen ook alles zien van de prototype van target
         if(Object.getPrototypeOf(target))showPrototypeChain(Object.getPrototypeOf(target), indention);
     }
 
+    // we willen de prototype chain zien van pietje
     showPrototypeChain(pietje, "");
+    // we willen de prototype chain zien van dog
     showPrototypeChain(dog, "");
 
     var bello = new Animal();
     showPrototypeChain(bello, "");
-    
+
 });
 
 
