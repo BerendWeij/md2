cd4143f59837fe63aa8e66a9358b603a60f35dea
Berend Weij <info@berendweij.nl>
2015-06-09 15:05:40 +0200

    added comments to angular example

--- examples/angular/app/controllers/ProjectController.js
+++ examples/angular/app/controllers/ProjectController.js
@@ -9,35 +9,22 @@
 
     function studentController($rootScope, $scope, $http) {
 
-        $scope.showBar = true;
-        $scope.editMode = false;
-
         // de view heeft ook toegang tot het $scope object
         // $scope is het centrale object tussen je view (HTML) en je controller
+        $scope.editMode = false;
+        $scope.currentDate = new Date();
         $scope.selectedStudent = "";
-
         // we geven het scope object een array met namen
-        $scope.students = [{name:"Mike"}, {name:"Lien"}, {name:"Steven"}, {name:"Isabelle"}, {name:"Rick"}];
+        $scope.stations = [];
 
         // de HTML mag bij deze functie omdat onze HTML ook bij $scope mag
         $scope.selectStudent = function(student){
             $scope.selectedStudent = student;
         }
 
-        // 1) -- ng-if - user profile, false, true
+        // met de ngAnimate module kunnen we elementen laten animeren binnen angular
 
-        // 2) -- ng-show - user profile
-
-        // 3) -- ng animate - user profile binnenhalen, script, module, css
-
-        // 4) -- ook voor data die in de array wijzigt
-
-        // 5) -- ng class - edit mode andere class
-
-        // 6) -- filter date (ook translate, etc.)
-        $scope.currentDate = new Date();
-
-        // 7) -- http json ophalen
+        // met de $http service kunnen we data laden
         $http.get("/md2/examples/angular/data/trainstations.json").then(function(result){
             $scope.students = result.data;
         });
